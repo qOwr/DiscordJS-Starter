25ebea573dd06270e1826876e1820a9d7fb6fa2d
qOwr <108898921+qOwr@users.noreply.github.com>
2022-07-28 19:36:31 +0300

    Changed spacing to make it more readable

--- src/index.js
+++ src/index.js
@@ -10,20 +10,20 @@ const client = new Client({
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
 }
 
 // Events
 const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	}
-	else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    }
+    else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
 }
 
 client.login(token);
