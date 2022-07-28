8ff3ba9402df0cead43e305675b3f9008139af4f
qOwr <108898921+qOwr@users.noreply.github.com>
2022-07-28 19:33:18 +0300

    Update deployCommands.js

--- src/helpers/deployCommands.js
+++ src/helpers/deployCommands.js
@@ -7,23 +7,23 @@ const commands = [];
 const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`../commands/${file}`);
-	commands.push(command.data.toJSON());
+    const command = require(`../commands/${file}`);
+    commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
-	try {
-		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId), // <-- Guild version
-			// Routes.applicationCommands(clientId), // <-- Global version
-			{ body: commands },
-		);
+    try {
+        await rest.put(
+        Routes.applicationGuildCommands(clientId, guildId), // <-- Guild version
+        // Routes.applicationCommands(clientId), // <-- Global version
+        { body: commands },
+    );
 
-		console.log('Successfully registered application commands.');
-	}
-	catch (error) {
-		console.error(`deployCommands.js error\n${error}`);
-	}
+    console.log('Successfully registered application commands.');
+    }
+    catch (error) {
+        console.error(`deployCommands.js error\n${error}`);
+    }
 })();
