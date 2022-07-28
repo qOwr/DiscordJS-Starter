f2b87c60f3223c067f0e7ccbe6887858a326328d
qOwr <108898921+qOwr@users.noreply.github.com>
2022-07-28 19:29:53 +0300

    Changed spacing to make it more readable

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,8 +1,8 @@
 module.exports = {
-	name: 'ready',
-	once: true,
-	async execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-		client.user.setActivity('/help');
-	},
+    name: 'ready',
+    once: true,
+    async execute(client) {
+        console.log(`Ready! Logged in as ${client.user.tag}`);
+        client.user.setActivity('/help');
+    },
 };
