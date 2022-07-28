445302d7e9ad481fda3bbeea9f1694446127e9b9
qOwr <108898921+qOwr@users.noreply.github.com>
2022-07-28 19:28:55 +0300

    Changed spacing to make it more readable

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,17 +1,17 @@
 module.exports = {
-	name: 'interactionCreate',
-	execute(interaction) {
-		if (!interaction.isCommand()) return;
+    name: 'interactionCreate',
+    execute(interaction) {
+        if (!interaction.isCommand()) return;
 
-		const command = interaction.client.commands.get(interaction.commandName);
+        const command = interaction.client.commands.get(interaction.commandName);
 
-		if (!command) return;
+        if (!command) return;
 
-		try {
-			command.execute(interaction);
-		}
-		catch (error) {
-			console.error(`interactionCreate error\n${interaction.commandName}\n${error}`);
-		}
-	},
+        try {
+            command.execute(interaction);
+        }
+        catch (error) {
+            console.error(`interactionCreate error\n${interaction.commandName}\n${error}`);
+        }
+    },
 };
