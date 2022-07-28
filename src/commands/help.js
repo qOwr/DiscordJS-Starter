b29c22fe137ecaa627a95d618f5d074d5f6250d5
qOwr <108898921+qOwr@users.noreply.github.com>
2022-07-28 19:24:20 +0300

    Changed spacing
    Changed spacing between lines and reduced inline spacing to make it easier for eyes

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,14 +2,15 @@ const { MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('help')
-		.setDescription('Prints a list of useful bot-related resources to chat.'),
-	async execute(interaction) {
-		const embed = new MessageEmbed()
-			.setTitle('Help')
-			.setDescription('N/A');
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Prints a list of useful bot-related resources to chat.'),
+	
+    async execute(interaction) {
+        const embed = new MessageEmbed()
+        .setTitle('Help')
+        .setDescription('N/A');
 
-		return interaction.reply({ embeds: [embed] });
-	},
+        return interaction.reply({ embeds: [embed] });
+    },
 };
